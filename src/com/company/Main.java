4f09e23be6d79266d4fdd7f4a463ed610b865af9
Ej-PC\Ej <eliazersantos.mail@gmail.com>
2017-03-09 03:07:29 +0800

    homework 201 program 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println(fahrenheitToCelcius(1));
+        System.out.println(minutesToYearsAndDays(525600 + 1440));
     }
 
 
@@ -12,6 +13,11 @@ public class Main {
         return  ((farenheit - 31 ) * 5) / 9 ;
     }
 
+    public static String minutesToYearsAndDays(int minutes) {
+        int years = minutes / 525600;   // there are 525600 minutes per 1 year
+        int days = (minutes % 525600) / 1440;  // there are 1440 minutes in 1 day
+        return "There are " + years + " year/s and " + days + " day/s";
+    }
 
 }
 
